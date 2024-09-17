004260058961914e85033a0c4fcc93db8b24aaf6
Roberto Tibes <66281839+Tibes1@users.noreply.github.com>
2024-09-17 20:03:21 -0300

    commit

---
+++ teste1/js.js
@@ -0,0 +1,20 @@
+var element = document.getElementById('arquivo');
+
+const button = document.getElementById('download')
+
+var opt = {
+    margin:       0,
+    filename:     'myfile.pdf',
+    image:        { type: 'jpeg', quality: 2 },
+    html2canvas:  { scale: 4},
+    jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
+};
+
+button.addEventListener("click", () => {
+    handleclick(element)
+    console.log('teste1')
+})
+
+function handleclick(element) {
+    html2pdf().set(opt).from(this.element).save();
+}
