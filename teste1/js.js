56591feceb1f620410e3d58b44c29381b62322a7
Roberto Tibes <66281839+Tibes1@users.noreply.github.com>
2024-10-03 18:09:05 -0300

    first_page_done

--- teste1/js.js
+++ teste1/js.js
@@ -5,7 +5,7 @@ const button = document.getElementById('download')
 var opt = {
     margin:       0,
     filename:     'myfile.pdf',
-    image:        { type: 'jpeg', quality: 2 },
+    image:        { type: 'jpeg', quality: 0.95 },
     html2canvas:  { scale: 4},
     jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
 };
