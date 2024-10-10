98eea7f08e56b740284fd944bc0ab736798afb93
Roberto Tibes <66281839+Tibes1@users.noreply.github.com>
2024-10-10 18:50:47 -0300

    codando sem mouse xDD

--- teste1/js.js
+++ teste1/js.js
@@ -1,6 +1,8 @@
-var element = document.getElementById('arquivo');
+var element1 = document.getElementById('arquivo1');
+var element2 = document.getElementById('arquivo2');
 
-const button = document.getElementById('download')
+const button1 = document.getElementById('download1')
+const button2 = document.getElementById('download2')
 
 var opt = {
     margin:       0,
@@ -10,11 +12,12 @@ var opt = {
     jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
 };
 
-button.addEventListener("click", () => {
-    handleclick(element)
+button1.addEventListener("click", () => {
+    html2pdf().set(opt).from(this.element1).save()
     console.log('teste1')
 })
+button2.addEventListener("click", () => {
+    html2pdf().set(opt).from(this.element2).save();
+    console.log('teste2')
+})
 
-function handleclick(element) {
-    html2pdf().set(opt).from(this.element).save();
-}
